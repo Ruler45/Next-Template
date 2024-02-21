ac0f0e13f49f220fa69227a2a692245e17796ce3
ruler45 <sahin0945@gmail.com>
2024-02-21 22:59:42 +0530

    base template configured

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const About = () => {
+    return ( 
+        <>
+        <h1>About page</h1>
+        <Link href="/" >Home</Link>
+        </>
+
+     );
+}
+ 
+export default About;
