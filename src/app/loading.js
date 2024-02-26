6894b70e8b9607fd44870eeb450611af01fc60a3
ruler45 <sahin0945@gmail.com>
2024-02-26 21:16:33 +0530

    Added Loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,5 +1,19 @@
+"use client";
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const loading = () => {
-  return <div className="loader">Loading...</div>;
+  return (
+    <div className="loader">
+      <Player
+        autoplay
+        loop
+        src="/lottie/loader.json"
+        style={{ height: "300px", width: "300px" }}
+      ></Player>
+    </div>
+  );
 };
 
 export default loading;
