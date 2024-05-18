245b97e13d96e34862a5f35d4d95bc8a650b3557
ruler45 <sahin0945@gmail.com>
2024-05-18 09:54:39 +0530

    UPD:removed lottiefiles and updated file structure

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,19 +1,7 @@
 "use client";
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Player } from "@lottiefiles/react-lottie-player";
-
 const loading = () => {
-  return (
-    <div className="loader">
-      <Player
-        autoplay
-        loop
-        src="/lottie/loader.json"
-        style={{ height: "300px", width: "300px" }}
-      ></Player>
-    </div>
-  );
+  return <div className="loader"></div>;
 };
 
 export default loading;
